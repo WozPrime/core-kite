60d60c3026bbf1f0cffe0dc0ef9754094ed9f715
raihanakhmadin <raihan.akhmadin@gmail.com>
2021-11-24 17:29:46 +0700

    Database seeder

--- database/seeders/InstancesModelSeeder.php
+++ database/seeders/InstancesModelSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\InstancesModel;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class InstancesModelSeeder extends Seeder
@@ -13,6 +15,15 @@ class InstancesModelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        InstancesModel::truncate();
+        InstancesModel::create([
+            'jenis_instansi' => 'Pemerintah'
+        ]);
+        InstancesModel::create([
+            'jenis_instansi' => 'Swasta'
+        ]);
+        InstancesModel::create([
+            'jenis_instansi' => 'Perorangan'
+        ]);
     }
 }
