c0d405edf5e4e032379c01ae55ffcbf03ec648b6
WozPrime <fakhri_ishan@student.untan.ac.id>
2021-12-06 16:00:39 +0700

    Grouping Route Middleware

--- routes/web.php
+++ routes/web.php
@@ -28,42 +28,43 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout']);
 
-// // ADMIN
-Route::get('/admin',[UserController::class, 'admin'])->name('admin')->middleware(['role','auth']);
-Route::get('/admin/profile/',[UserController::class, 'profile'])->name('profile')->middleware(['role','auth']);
-Route::get('/admin/tables',[UserController::class, 'tables'])->name('tables')->middleware(['role','auth']);
-Route::get('/admin/profile',[UserController::class, 'profile'])->name('profile')->middleware(['role','auth']);
-// Route::get('/admin/klien',[UserController::class, 'klien'])->name('klien')->middleware(['role','auth']);
-// Route::get('/admin/klien/detail',[UserController::class, 'detailklien'])->name('detailklien')->middleware(['role','auth']);
-
-//KLIEN
-Route::resource('/admin/instansi', InstanceController::class)->middleware(['role', 'auth']);
-
-//profile
-Route::get('/admin/profile/{id}',[UserController::class, 'profile'])->name('profile')->middleware(['role','auth']);
-Route::post('/admin/profile/edit/{id}',[UserController::class, 'edit'])->name('edit')->middleware(['role','auth']);
-Route::post('/admin/profile/edit2/{id}',[UserController::class, 'edit2'])->name('edit2')->middleware(['role','auth']);
-Route::get('/admin/profile/delete/{id}',[UserController::class, 'delete_user'])->name('delete')->middleware(['role','auth']);
-Route::post('/admin/profile/cpass/{id}',[UserController::class, 'cpass'])->name('cpass')->middleware(['role','auth']);
-Route::get('/admin/manage_user/',[UserController::class, 'manage_user'])->name('manage_user')->middleware(['role','auth']);
-
-
-//Profession
-Route::get('/admin/prof/',[ProfController::class, 'index'])->name('prof')->middleware(['role','auth']);
-Route::post('/admin/ins_prof/',[ProfController::class, 'create'])->name('ins_prof')->middleware(['role','auth']);
-Route::post('/admin/edit_prof/{id}',[ProfController::class, 'edit'])->name('edit_prof')->middleware(['role','auth']);
-Route::get('/admin/delete_prof/{id}',[ProfController::class, 'delete'])->name('delete_prof')->middleware(['role','auth']);
-
-//Joblist
-Route::get('/admin/joblist/', [PostController::class,'show'])->name('joblist')->middleware(['role','auth']);
-Route::post('/admin/ins_post/',[PostController::class, 'create'])->name('ins_post')->middleware(['role','auth']);
-Route::post('/admin/edit_post/{id}',[PostController::class, 'edit'])->name('edit_post')->middleware(['role','auth']);
-Route::get('/admin/delete_post/{id}',[PostController::class, 'delete'])->name('delete_post')->middleware(['role','auth']);
-
-//JobData
 Route::middleware(['role','auth'])->group(function ()
 {
+    // // ADMIN
+    Route::get('/admin',[UserController::class, 'admin'])->name('admin');
+    Route::get('/admin/profile/',[UserController::class, 'profile'])->name('profile');
+    Route::get('/admin/tables',[UserController::class, 'tables'])->name('tables');
+    Route::get('/admin/profile',[UserController::class, 'profile'])->name('profile');
+    // Route::get('/admin/klien',[UserController::class, 'klien'])->name('klien')
+    // Route::get('/admin/klien/detail',[UserController::class, 'detailklien'])->name('detailklien')
+
+    //KLIEN
+    Route::resource('/admin/instansi', InstanceController::class);
+
+    //profile
+    Route::get('/admin/profile/{id}',[UserController::class, 'profile'])->name('profile');
+    Route::post('/admin/profile/edit/{id}',[UserController::class, 'edit'])->name('edit');
+    Route::post('/admin/profile/edit2/{id}',[UserController::class, 'edit2'])->name('edit2');
+    Route::get('/admin/profile/delete/{id}',[UserController::class, 'delete_user'])->name('delete');
+    Route::post('/admin/profile/cpass/{id}',[UserController::class, 'cpass'])->name('cpass');
+    Route::get('/admin/manage_user/',[UserController::class, 'manage_user'])->name('manage_user');
+
+
+    //Profession
+    Route::get('/admin/prof/',[ProfController::class, 'index'])->name('prof');
+    Route::post('/admin/ins_prof/',[ProfController::class, 'create'])->name('ins_prof');
+    Route::post('/admin/edit_prof/{id}',[ProfController::class, 'edit'])->name('edit_prof');
+    Route::get('/admin/delete_prof/{id}',[ProfController::class, 'delete'])->name('delete_prof');
+
+    //Joblist
+    Route::get('/admin/joblist/', [PostController::class,'show'])->name('joblist');
+    Route::post('/admin/ins_post/',[PostController::class, 'create'])->name('ins_post');
+    Route::post('/admin/edit_post/{id}',[PostController::class, 'edit'])->name('edit_post');
+    Route::get('/admin/delete_post/{id}',[PostController::class, 'delete'])->name('delete_post');
+
+    //JobData
     Route::resource('admin/jobdata/', JobDataController::class);
+
 });
 
 // EMPLOYEE
