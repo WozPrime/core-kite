16d446a6217775e85c1baa07611a651f757cbf5e
Husni Ramadhan <husniramadhan@student.untan.ac.id>
2021-12-16 17:02:01 +0700

    Implementasi CSS AdminLTE

--- resources/views/pages/emp/empreport.blade.php
+++ resources/views/pages/emp/empreport.blade.php
@@ -4,58 +4,27 @@
 @endsection
 @section('emphead')
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <style>
-        .floating-btn {
-            width: 50px;
-            height: 50px;
-            background: var(--gray-dark);
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            text-decoration: none;
-            border-radius: 50%;
-            color: var(--white);
-            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.25);
-            position: fixed;
-            right: 20px;
-            bottom: 20px;
-            transition: background 0.25s;
-    
-            /* button */
-            outline: gray;
-            border: none;
-            cursor: pointer;
-        }
-    
-        .floating-btn:hover {
-            color: lawngreen;
-        }
-    
-        .floating-btn:active {
-            background: var(--gray);
-        }
-    </style>
 @endsection
 @section('empscript')
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
     <script>
-        $(function () {
-          $("#example1").DataTable({
-            "responsive": true, "lengthChange": false, "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-          }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-          $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-          });
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
         });
-      </script>
+    </script>
 @endsection
 
 <style>
@@ -92,67 +61,103 @@
 </style>
 
 @section('content')
-<section class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-9 center">
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Table Laporan</h3>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-9 center">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title pt-1">List of Reports</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool pt-3" data-card-widget="collapse" title="Collapse">
+                                    <i class="fa fa-minus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table class="table table-responsive-sm table-bordered" id="myTable" width="100%">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px">No</th>
+                                        <th class="col-2">Status</th>
+                                        <th class="col-3">Date</th>
+                                        <th class="col-2">Job</th>
+                                        <th class="col-2">Project Name</th>
+                                        <th class="col-2">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data as $tbl_report)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td></td>
+                                            <td>{{ $tbl_report->report_status }}</td>
+                                            <td>{{ $tbl_report->report_date }}</td>
+                                            <td></td>
+                                            <td>{{ $tbl_report->project_name }}</td>
+                                            <td style="text-align: center">
+                                                <a class="btn btn-primary"
+                                                    href="/admin/proyek/{{ $tbl_project->report_id }}"><i
+                                                        class="fa fa-eye"></i></a>
+                                                <a class="btn btn-success" data-toggle="modal"
+                                                    href="#edit{{ $tbl_project->report_id }}"><i
+                                                        class="fa fa-edit"></i></a>
+                                                <a class="btn btn-danger" data-toggle="modal"
+                                                    href="#delete{{ $tbl_project->report_id }}"><i
+                                                        class="fa fa-trash"></i></a>
+                                            </td>
+                                            <!-- /.modal -->
+                                            <div class="modal fade" id="edit{{ $tbl_project->report_id }}">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="card-header bg-orange">
+                                                            <h3 class="card-title">Add New Project Data</h3>
+                                                        </div>
+                                                        <div class="card-body">
+                                                            <form action="/admin/projects/" method="post"
+                                                                enctype="multipart/form-data">
+                                                                @csrf
+
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                        <!-- /.card -->
+                        <!-- /.card-body -->
+                    </div>
+                </div>
             </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>Rendering engine</th>
-                  <th>Browser</th>
-                  <th>Platform(s)</th>
-                  <th>Engine version</th>
-                  <th>CSS grade</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>Gecko</td>
-                  <td>Seamonkey 1.1</td>
-                  <td>Win 98+ / OSX.2+</td>
-                  <td>1.8</td>
-                  <td>A</td>
-                </tr>
-                <tr>
-                  <td>Gecko</td>
-                  <td>Epiphany 2.20</td>
-                  <td>Gnome</td>
-                  <td>1.8</td>
-                  <td>A</td>
-                </tr>
-                <tr>
-                  <td>Webkit</td>
-                  <td>Safari 1.2</td>
-                  <td>OSX.3</td>
-                  <td>125.5</td>
-                  <td>A</td>
-                </tr>
-                <tr>
-                  <td>Webkit</td>
-                  <td>Safari 1.3</td>
-                  <td>OSX.3</td>
-                  <td>312.8</td>
-                  <td>A</td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
         </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+    <button class="material-icons floating-btn" data-toggle="modal" data-target="#add-data">add</button>
+    <div class="modal fade" id="add-data" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+
+            <div class="modal-content card-primary">
+                <div class="card-header">
+                    <h4 class="card-title">Modal title</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+                </div>
+
+                <div class="card-body">
+
+                </div>
+
+                <div class="modal-footer">
+                    <!--Nothing Goes Here but is needed! -->
+                </div>
+            </div>
+        </div>
     </div>
-    <!-- /.container-fluid -->
-  </section>
 @endsection
 <button class="material-icons floating-btn" data-toggle="modal" data-target="#add-report">add</button>
