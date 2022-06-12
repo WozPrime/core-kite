fa42ff0d22918e4edd139741e11fb94786c5af0d
Husni Ramadhan <husniramadhan@student.untan.ac.id>
2022-06-12 16:10:34 +0700

    Minor Fix Seeder

--- database/seeders/ProjectTaskSeeder.php
+++ database/seeders/ProjectTaskSeeder.php
@@ -22,16 +22,16 @@ class ProjectTaskSeeder extends Seeder
             'details' => 'Tugas Test Seeder',
             'status' => '2',
             'points' => '5',
-            'start_at' => '2021-12-12 12:12:00',
-            'expired_at' => '2022-12-12 12:12:00'
+            'start_at' => '2021-4-12 12:12:00',
+            'expired_at' => '2022-8-12 12:12:00'
         ]);
         ProjectTask::create([
             'task_id' => '3',
             'user_id' => '8',
             'project_id' => '2',
             'details' => 'Tugas Test Seeder 2',
-            'start_at' => '2021-12-12 12:12:00',
-            'expired_at' => '2022-12-12 12:12:00'
+            'start_at' => '2022-5-12 12:12:00',
+            'expired_at' => '2022-7-12 12:12:00'
         ]);
     }
 }
